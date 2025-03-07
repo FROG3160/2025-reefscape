339903435193be23068d0e43c21c7a9b7583111e
Joseph Wang <joseph007wang@gmail.com>
2025-03-06 18:28:06 -0600

    Created vision class for color targets.

--- roborio/subsystems/vision.py
+++ roborio/subsystems/vision.py
@@ -21,3 +21,14 @@ class VisionPose:
     def periodic(self):
         self.latestVisionPose = self.estimator.update()
         return self.latestVisionPose
+
+
+class ColorVision:
+    def __init__(self, colorCameraName: str):
+        self.camera = PhotonCamera(colorCameraName)
+
+    def periodic(self):
+        self.latestTargetResult = self.camera.getLatestResult()
+        self.latestTargets = self.latestTargetResult.getTargets()
+        for target in self.latestTargets:
+            pass
