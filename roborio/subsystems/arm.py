b9a8fca9b02b4738aa12705f795e82d6ed109dce
Brian Jarrett <celttechie@gmail.com>
2025-03-10 21:04:37 -0500

    Added homing function, software limits, updating gearing

--- roborio/subsystems/arm.py
+++ roborio/subsystems/arm.py
@@ -1,11 +1,17 @@
 from commands2.subsystem import Subsystem
 from FROGlib.ctre import FROGTalonFX, FROGTalonFXConfig, FROGFeedbackConfig
 import constants
-from phoenix6.configs import Slot0Configs, Slot1Configs, MotorOutputConfigs
-from phoenix6.signals import NeutralModeValue
+from phoenix6.configs import (
+    Slot0Configs,
+    Slot1Configs,
+    MotorOutputConfigs,
+    SoftwareLimitSwitchConfigs,
+)
+from phoenix6.signals import NeutralModeValue, InvertedValue
 from phoenix6.controls import Follower, VelocityVoltage, PositionVoltage, VoltageOut
 from typing import Callable
 from commands2 import Command
+from configs.ctre import motorOutputCWPandBrake
 
 
 class Arm(Subsystem):
@@ -13,15 +19,21 @@ class Arm(Subsystem):
         self.motor = FROGTalonFX(
             id=constants.kArmMotorID,
             motor_config=FROGTalonFXConfig(
-                feedback_config=FROGFeedbackConfig().with_sensor_to_mechanism_ratio(90),
+                feedback_config=FROGFeedbackConfig().with_sensor_to_mechanism_ratio(16),
                 slot0gains=Slot0Configs(),
-            ).with_motor_output(
-                MotorOutputConfigs().with_neutral_mode(NeutralModeValue.BRAKE)
+            )
+            .with_motor_output(motorOutputCWPandBrake)
+            .with_software_limit_switch(
+                SoftwareLimitSwitchConfigs()
+                .with_forward_soft_limit_enable(True)
+                .with_forward_soft_limit_threshold(8)
             ),
             parent_nt="Arm",
             motor_name="motor",
         )
         self.limitswitch = None
+        self.homing_voltage = -0.25  # motor is inverted, negative values retract
+        self.homing_torque_limit = 8.0
 
     def joystick_retract_command(self, retract_control: Callable[[], float]) -> Command:
         """Returns a command that takes a joystick control giving values between
@@ -29,7 +41,7 @@ class Arm(Subsystem):
 
         Args:
             control (Callable[[], float]): A control from the joystick that provides
-            a value from -1.0 to 1.0
+            a value from 0 to 1.0
 
         Returns:
             Command: The command that will cause the motor to move from joystick control.
@@ -46,7 +58,7 @@ class Arm(Subsystem):
 
         Args:
             control (Callable[[], float]): A control from the joystick that provides
-            a value from -1.0 to 1.0
+            a value from 0 to 1.0
 
         Returns:
             Command: The command that will cause the motor to move from joystick control.
@@ -55,4 +67,27 @@ class Arm(Subsystem):
             lambda: self.motor.set_control(
                 VoltageOut(extend_control() * 4, enable_foc=False)
             )
+        )
+
+    def reset_position(self):
+        self.motor.set_position(0)
+
+    def get_torque(self):
+        return self.motor.get_torque_current().value
+
+    def stop_homing(self):
+        return abs(self.get_torque()) > self.homing_torque_limit
+
+    def set_home(self) -> Command:
+        return (
+            self.startEnd(
+                # START
+                lambda: self.motor.set_control(
+                    VoltageOut(self.homing_voltage, enable_foc=False)
+                ),
+                # END
+                lambda: self.motor.set_control(VoltageOut(0, enable_foc=False)),
+            )
+            .until(self.stop_homing)
+            .andThen(self.runOnce(self.reset_position))
         )
