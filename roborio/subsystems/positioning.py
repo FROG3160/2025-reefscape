9872717a270352e6b29f9935d2173f1150cb6e26
Brian Jarrett <celttechie@gmail.com>
2025-03-18 19:23:18 -0500

    attempts to calculate position against reef

--- roborio/subsystems/positioning.py
+++ roborio/subsystems/positioning.py
@@ -44,6 +44,34 @@ class Position(FROGField):
     def setReefTags(self, alliance: DriverStation.Alliance):
         self._reef_tags = kReefTags[alliance]["Reef"]
 
+    def getRightSidePose():
+        import math
+
+        # distance from center of robot forward to the centerline of the arm swing
+        robot_forward_adjust = 3  # 5 centimeters
+        robot_left_adjust = (
+            -12.5
+        )  # half the width of the robot, amounts to distance to the bumber's edge
+        stem_offset = 6.5  # the distance from center of the apriltag to the reef stem
+        # this moves the aimpoint to the edge of the robot that we need to match
+        # to one of the reef stems
+        robot_aim_point = Transform3d(
+            Translation3d(-robot_forward_adjust, -robot_left_adjust, 0),
+            Rotation3d(0, 0, -math.pi / 2),
+        )
+        rightside_stem_adjust = Transform3d(
+            Translation3d(-robot_left_adjust, -stem_offset + robot_forward_adjust, 0),
+            Rotation3d(),
+        )
+        leftside_stem_adjust = Transform3d(
+            Translation3d(-robot_left_adjust, stem_offset + robot_forward_adjust, 0),
+            Rotation3d(),
+        )
+        test = Pose3d(0, 0, 0, Rotation3d()).transformBy(rightside_stem_adjust)
+        test2 = Pose3d(0, 0, 0, Rotation3d()).transformBy(leftside_stem_adjust)
+        print(test)
+        print(test2)
+
     # print(f"    ANGLE: {}")
 
 
