7ded99d1350946cc7c57565f64d26c1f9995086e
Brian Jarrett <celttechie@gmail.com>
2025-03-18 19:23:53 -0500

    Added method to give vision ambiguity

--- roborio/subsystems/vision.py
+++ roborio/subsystems/vision.py
@@ -1,5 +1,6 @@
 from photonlibpy import PhotonCamera, PhotonPoseEstimator, PoseStrategy
 from wpimath.geometry import Transform3d
+from wpilib import SmartDashboard
 
 from robotpy_apriltag import AprilTagField, AprilTagFieldLayout
 
@@ -17,6 +18,19 @@ class VisionPose:
             camera=PhotonCamera(poseCameraName),
             robotToCamera=cameraTransform,
         )
+
+    def get_result(self):
+        estimated_pose = self.estimator.update()
+        if estimated_pose:
+            targets = estimated_pose.targetsUsed[0].getBestCameraToTarget()
+            SmartDashboard.putNumber(
+                f"{self.estimator._camera.getName()} Targets Found", len(targets)
+            )
+            target1 = targets[0]
+            SmartDashboard.putNumber(
+                f"{self.estimator._camera.getName()} First Target Ambiguity",
+                target1.getPoseAmbiguity(),
+            )
 
     def periodic(self):
         self.latestVisionPose = self.estimator.update()
