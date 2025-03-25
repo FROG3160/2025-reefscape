53591e01fcd16fd891c79dac09dfa0a65eed4fd8
Brian Jarrett <celttechie@gmail.com>
2025-03-24 19:01:47 -0500

    commented out unneeded grabber methods

--- roborio/subsystems/grabber.py
+++ roborio/subsystems/grabber.py
@@ -167,27 +167,27 @@ class Grabber(Subsystem):
                 )
             )
 
-    def intake_algae(self) -> Command:
-        return self.runOnce(
-            # start running the motor
-            lambda: self.motor.set_control(self.motor_intake),
-        )
+    # def intake_algae(self) -> Command:
+    #     return self.runOnce(
+    #         # start running the motor
+    #         lambda: self.motor.set_control(self.motor_intake),
+    #     )
 
-    def eject_algae(self) -> Command:
-        return self.runOnce(
-            # start running the motor
-            lambda: self.motor.set_control(-self.motor_eject),
-        ).until(self.not_detecting_algae)
+    # def eject_algae(self) -> Command:
+    #     return self.runOnce(
+    #         # start running the motor
+    #         lambda: self.motor.set_control(-self.motor_eject),
+    #     ).until(self.not_detecting_algae)
 
     def run_motor(self, voltage) -> Command:
         return self.runOnce(
             lambda: self.motor.set_control(VoltageOut(voltage, enable_foc=False))
         )
 
-    def run_motor_with_variable(self, callable: Callable[[], float]) -> Command:
-        return self.runOnce(
-            lambda: self.motor.set_control(VoltageOut(callable(), enable_foc=False))
-        )
+    # def run_motor_with_variable(self, callable: Callable[[], float]) -> Command:
+    #     return self.runOnce(
+    #         lambda: self.motor.set_control(VoltageOut(callable(), enable_foc=False))
+    #     )
 
     def stop_motor(self) -> Command:
         return self.runOnce(self.motor.stopMotor())
