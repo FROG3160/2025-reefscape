bd238e53f9fe8f087fd0b09cd64bf1cd8ff9e978
Joseph Wang <joseph007wang@gmail.com>
2025-03-24 20:28:20 -0500

    added alongWith to extend arm to 3 when deploying the climber

--- roborio/robotcontainer.py
+++ roborio/robotcontainer.py
@@ -431,6 +431,7 @@ class RobotContainer:
             self.elevator.move(3)
             .alongWith(self.shoulder.move(0))
             .andThen(self.climber.deploy_climber(1.7))
+            .alongWith(self.arm.move(3))
         )
         self.tacticalController.start().onTrue(self.climber.run_motor())
         self.tacticalController.a().onTrue(
