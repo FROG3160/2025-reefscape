b222dc1bdd18c6c40dff209805c28f60993a094e
Brian Jarrett <celttechie@gmail.com>
2025-03-27 15:00:08 -0500

    added telemetry for climber

--- roborio/subsystems/climber.py
+++ roborio/subsystems/climber.py
@@ -41,9 +41,30 @@ class Climber(Subsystem):
             parent_nt="Climber",
             motor_name="motor",
         )
-
         self.motor_winch_request = VoltageOut(output=2.5, enable_foc=False)
         self.motor_deploy_request = VoltageOut(output=2.0, enable_foc=False)
+
+        nt_table = f"Subsystems/{self.__class__.__name__}"
+        self._torque_current_pub = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(f"{nt_table}/torque_current")
+            .publish()
+        )
+        self._position_pub = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(f"{nt_table}/position")
+            .publish()
+        )
+        self._voltage_pub = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(f"{nt_table}/voltage")
+            .publish()
+        )
+        self._limit_switch_pub = (
+            NetworkTableInstance.getDefault()
+            .getBooleanTopic(f"{nt_table}/limit_switch")
+            .publish()
+        )
 
     def run_motor(self) -> Command:
         return self.runOnce(lambda: self.motor.set_control(self.motor_winch_request))
@@ -57,3 +78,9 @@ class Climber(Subsystem):
             .andThen(WaitCommand(time))
             .andThen(self.stop_motor())
         )
+
+    def periodic(self):
+        self._position_pub.set(self.motor.get_position())
+        self._torque_current_pub.set(self.motor.get_torque_current())
+        self._voltage_pub.set(self.motor.get_motor_voltage())
+        self._limit_switch_pub.set(self.motor.get_forward_limit())
